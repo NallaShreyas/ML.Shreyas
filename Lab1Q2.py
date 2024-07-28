0f17d9eeec3143a3897ca180635f91ed83bdb0f7
NallaShreyas <shreyasnalla08@gmail.com>
2024-07-28 22:05:22 +0530

    lab1q2

---
+++ Lab1Q2.py
@@ -0,0 +1,19 @@
+list2=[]
+size = int(input("Enter the number of elements in list :"))
+if size < 3:
+    print("Range Determination not possible...")
+else:
+    for i in range(size):
+        item = int(input("Enter element :"))
+        list2.append(item)
+    print("Your list is ",list2)
+#finding min  & max element
+mini = list2[0]
+maxi = list2[0]
+for j in range(len(list2)):
+    if list2[j] < mini:
+        mini = list2[j]
+    elif list2[j] > maxi:
+        maxi = list2[j]
+range = maxi - mini
+print("Range of given list is:",range)
