1b96871e4df4cf3f73f556bea28bffe315b30572
NallaShreyas <shreyasnalla08@gmail.com>
2024-08-04 23:20:34 +0530

    Update Lab2Q1A1.py

--- Lab2Q1A1.py
+++ Lab2Q1A1.py
@@ -1,23 +1,45 @@
 import pandas as pd
-
-file_path = 'C:\Users\shrey\Downloads\Lab Session Data.xlsx'
-sheet_name = 'Grocery'
-data = pd.read_excel(file_path,sheet_name=sheet_name)
-#####
-A = data[['Feature1', 'Feature2', 'FeatureN']].values
-C = data[['Price']].values
-#####
-dimensionality = A.shape[1]
-print(f"Dimensionality of vector space :{dimensionality}")
-#########
-num_vectors = A.shape[0]
-printf(f"Number of vectors in vector space: {num_vectors}")
-#####
 import numpy as np
-rank_A = np.linalg.matrix_rank(A)
-print(f"Rank of amtrix A:{rank_A} ")
-#####
-A_pseudo_inv = np.linalg.pinv(A)
 
-estimated_costs = A_pseudo_inv @ C
-print(f"Estimated cost of each product : {estimated_costs}")
+# Use forward slashes in the file path
+s = "C:\Users\shrey\Downloads\Lab Session Data.xlsx"
+
+# Load the correct sheet name
+data = pd.read_excel(s, sheet_name='Purchase data')
+
+# Extract relevant columns for matrix A
+A = data[['Candies (#)', 'Mangoes (Kg)','Milk Packets (#)']].dropna()
+
+# Extract relevant columns for matrix C
+C=data[['Payment (Rs)']].dropna()
+
+# Convert to NumPy array
+A_matrix = A.to_numpy()
+
+# Convert to NumPy array
+C_MAtrix=C.to_numpy()
+
+# Print the matrix
+row=0
+col=0
+print(A_matrix)
+for i in A_matrix:
+    row=row+1
+print(row)
+for j in A_matrix[0]:
+    col=col+1
+print(col)
+print(f"The Matrix A is {row}X{col}")
+print(f"The number of vectors the exist in the vector space are {row}")
+
+rank_A = np.linalg.matrix_rank(A_matrix)
+print(f'The rank of the matrix is',rank_A)
+
+inverse_A = np.linalg.pinv(A_matrix)
+X_matrix = np.dot(inverse_A, C_MAtrix)
+print(X_matrix)
+
+print(f"Cost of each  candy :", X_matrix[0])
+print(f"Cost of each  Mango :", X_matrix[1])
+print(f"Cost of each  milk packet :", X_matrix[2])
+
